ced6d4f33a0b8d303dd73b0febf09a2e18eaeec2
blk14 <bogdan.cris99@yahoo.com>
2019-08-07 16:22:52 +0300

    hobbies almost done

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 
 
-let getSlides = document.querySelectorAll(".slide");
-let index = 0;
-getSlides[0].style.display = "block";
-console.log(getSlides);
+let getSlidesBasket = document.querySelectorAll(".slide-basket");
+let getSlidesFood = document.querySelectorAll(".slide-food");
+let getFoodDesc = document.querySelectorAll(".food-desc");
+let getSlidesSki = document.querySelectorAll(".slide-ski");
+
+
+let indexBasket = 0;
+let indexFood = 0;
+let indexSki = 0;
+getSlidesBasket[0].style.display = "block";
+getSlidesFood[0].style.display = "block";
+getFoodDesc[0].style.display = "block";
+getSlidesSki[0].style.display = "block";
+
 
 
 // functie care imi intoarce indexul in functie de prev sau next
-let getIndexFunction = function(index, scrollOrientation) {
-
+// si categoria de slide-uri din care face parte
+let getIndexFunction = function(index, scrollOrientation, getSlides) {
     if (scrollOrientation == 0) {
         if (index == getSlides.length - 1) {
             return 0;
@@ -26,11 +36,37 @@ let getIndexFunction = function(index, scrollOrientation) {
 }
 
 
-let scrollFunction = function(scrollOrientation) {
-    console.log("vechiul  " + index);
+let scrollFunction = function(scrollOrientation, slidesName) {
+    let getSlides;
+    let index;
+    if (slidesName == "basket") {
+        getSlides = getSlidesBasket;
+        index = indexBasket;
+    } else if (slidesName == "food") {
+        getSlides = getSlidesFood;
+        index = indexFood;
+        getFoodDesc[index].style.display = "none";
+    } else if (slidesName == "ski") {
+        getSlides = getSlidesSki;
+        index = indexSki;
+    }
+
     getSlides[index].style.display = "none";
-    let newIndex = getIndexFunction(index, scrollOrientation);
+    let newIndex = getIndexFunction(index, scrollOrientation, getSlides);
+
     index = newIndex;
     getSlides[index].style.display = "block";
-    console.log("noul "  + index);
+
+    if (slidesName == "food") {
+        getFoodDesc[index].style.display = "block";
+    }
+
+    if (slidesName == "basket") {
+        indexBasket = newIndex;
+    } else if (slidesName == "food") {
+        indexFood = newIndex;
+    } else if (slidesName == "ski") {
+        indexSki = newIndex;
+    }
+
 }
