5fdf8cc8c705405fe677ab60fc391091481498d6
blk14 <bogdan.cris99@yahoo.com>
2019-08-08 00:20:12 +0300

    hobbies non-responsive

--- main.js
+++ main.js
@@ -4,15 +4,25 @@ let getSlidesBasket = document.querySelectorAll(".slide-basket");
 let getSlidesFood = document.querySelectorAll(".slide-food");
 let getFoodDesc = document.querySelectorAll(".food-desc");
 let getSlidesSki = document.querySelectorAll(".slide-ski");
-
+let getSlidesTravel = document.querySelectorAll(".slide-travel");
+let getTravelDesc = document.querySelectorAll(".travel-desc");
 
 let indexBasket = 0;
 let indexFood = 0;
 let indexSki = 0;
+let indexTravel = 0;
+
 getSlidesBasket[0].style.display = "block";
+
 getSlidesFood[0].style.display = "block";
 getFoodDesc[0].style.display = "block";
+
 getSlidesSki[0].style.display = "block";
+
+getSlidesTravel[0].style.display = "block";
+getTravelDesc[0].style.display = "block";
+
+console.log(getSlidesTravel);
 
 
 
@@ -39,6 +49,9 @@ let getIndexFunction = function(index, scrollOrientation, getSlides) {
 let scrollFunction = function(scrollOrientation, slidesName) {
     let getSlides;
     let index;
+
+    // setez slide-urile care trebuie scroluite,
+    // si descrierea lor la food si travel
     if (slidesName == "basket") {
         getSlides = getSlidesBasket;
         index = indexBasket;
@@ -49,8 +62,14 @@ let scrollFunction = function(scrollOrientation, slidesName) {
     } else if (slidesName == "ski") {
         getSlides = getSlidesSki;
         index = indexSki;
+    } else if (slidesName == "travel") {
+        getSlides = getSlidesTravel;
+        index = indexTravel;
+        getTravelDesc[index].style.display = "none";
     }
 
+    // aflu noul index, apoi setez display block la slides,
+    // si la descriere unde e cazul
     getSlides[index].style.display = "none";
     let newIndex = getIndexFunction(index, scrollOrientation, getSlides);
 
@@ -59,6 +78,8 @@ let scrollFunction = function(scrollOrientation, slidesName) {
 
     if (slidesName == "food") {
         getFoodDesc[index].style.display = "block";
+    } else if (slidesName == "travel") {
+        getTravelDesc[index].style.display = "block";
     }
 
     if (slidesName == "basket") {
@@ -67,6 +88,8 @@ let scrollFunction = function(scrollOrientation, slidesName) {
         indexFood = newIndex;
     } else if (slidesName == "ski") {
         indexSki = newIndex;
+    } else if (slidesName == "travel") {
+        indexTravel = newIndex;
     }
 
 }
